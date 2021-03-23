f56760870e7d5f02bc213de387000fd52fcc1c0b
nessye <69359764+nessye@users.noreply.github.com>
2021-03-23 02:17:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ if __name__ == "__main__":
     for modulo in modulos:
         client.load_extension(modulo)
 
-    client.run("ODIxNTgxMTEwMjA2MDA1MjUw.YFFzCA.4y0aTZs631ZCh2PRrUDjLgZtUdQ")
+    client.run("ODIxNTgxMTEwMjA2MDA1MjUw.YFFzCA.UlCM98BUi_4Rei0jhqw2-WEWNR8")
